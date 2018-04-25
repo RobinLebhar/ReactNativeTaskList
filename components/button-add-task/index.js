672b026c8d4fb7dc3088a55497325f857a527efb
Robin Lebhar <robin_lebhar@hotmail.fr>
2018-04-25 19:10:58 -0400

    Update index.js

--- components/button-add-task/index.js
+++ components/button-add-task/index.js
@@ -5,10 +5,10 @@ import { APP_COLORS } from '../../styles/color';
 
 const ButtonAddTask = ({ onPressCallBack }) => (
   <ActionButton
- buttonColor={APP_COLORS.primaryAction}
- onPress={() => onPressCallBack()}
- >
- <Icon color={APP_COLORS.primaryText} name={'add'} />
+    buttonColor={APP_COLORS.primaryAction}
+    onPress={() => onPressCallBack()}
+  >
+    <Icon color={APP_COLORS.primaryText} name={'add'} />
  </ActionButton>
 );
 
